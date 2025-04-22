ffdc3a5fea25cedea6ea5c30aa4388a1041cbf10
dsscaze <dsscaze@gmail.com>
2025-04-22 09:23:19 -0300

    packages updates

---
+++ DotNetTips/DotNetTips.2022/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace DotNetTips._2022
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVermelho = new System.Windows.Forms.Button();
+            this.btnVerde = new System.Windows.Forms.Button();
+            this.btnAmarelo = new System.Windows.Forms.Button();
+            this.btnAzul = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lbJogadorVez = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnVermelho
+            // 
+            this.btnVermelho.BackColor = System.Drawing.Color.Red;
+            this.btnVermelho.Location = new System.Drawing.Point(495, 106);
+            this.btnVermelho.Name = "btnVermelho";
+            this.btnVermelho.Size = new System.Drawing.Size(134, 93);
+            this.btnVermelho.TabIndex = 1;
+            this.btnVermelho.UseVisualStyleBackColor = false;
+            this.btnVermelho.Click += new System.EventHandler(this.btnVermelho_Click);
+            // 
+            // btnVerde
+            // 
+            this.btnVerde.BackColor = System.Drawing.Color.Green;
+            this.btnVerde.Location = new System.Drawing.Point(355, 106);
+            this.btnVerde.Name = "btnVerde";
+            this.btnVerde.Size = new System.Drawing.Size(134, 93);
+            this.btnVerde.TabIndex = 2;
+            this.btnVerde.UseVisualStyleBackColor = false;
+            this.btnVerde.Click += new System.EventHandler(this.btnVerde_Click);
+            // 
+            // btnAmarelo
+            // 
+            this.btnAmarelo.BackColor = System.Drawing.Color.Yellow;
+            this.btnAmarelo.Location = new System.Drawing.Point(355, 205);
+            this.btnAmarelo.Name = "btnAmarelo";
+            this.btnAmarelo.Size = new System.Drawing.Size(134, 93);
+            this.btnAmarelo.TabIndex = 3;
+            this.btnAmarelo.UseVisualStyleBackColor = false;
+            this.btnAmarelo.Click += new System.EventHandler(this.btnAmarelo_Click);
+            // 
+            // btnAzul
+            // 
+            this.btnAzul.BackColor = System.Drawing.Color.Blue;
+            this.btnAzul.Location = new System.Drawing.Point(495, 205);
+            this.btnAzul.Name = "btnAzul";
+            this.btnAzul.Size = new System.Drawing.Size(134, 93);
+            this.btnAzul.TabIndex = 4;
+            this.btnAzul.UseVisualStyleBackColor = false;
+            this.btnAzul.Click += new System.EventHandler(this.btnAzul_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(38, 106);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 42);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "NOVO JOGO";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbJogadorVez
+            // 
+            this.lbJogadorVez.AutoSize = true;
+            this.lbJogadorVez.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbJogadorVez.Location = new System.Drawing.Point(31, 390);
+            this.lbJogadorVez.Name = "lbJogadorVez";
+            this.lbJogadorVez.Size = new System.Drawing.Size(199, 39);
+            this.lbJogadorVez.TabIndex = 6;
+            this.lbJogadorVez.Text = "JogadorVez";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbJogadorVez);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnAzul);
+            this.Controls.Add(this.btnAmarelo);
+            this.Controls.Add(this.btnVerde);
+            this.Controls.Add(this.btnVermelho);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVermelho;
+        private System.Windows.Forms.Button btnVerde;
+        private System.Windows.Forms.Button btnAmarelo;
+        private System.Windows.Forms.Button btnAzul;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lbJogadorVez;
+    }
+}
+
---
+++ DotNetTips/DotNetTips.2022/Form1.cs
@@ -0,0 +1,231 @@
+﻿using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace DotNetTips._2022
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private const double FrequenciaBotaoVerde = 329.628; // nota E uma oitava abaixo
+        private const double FrequenciaBotaoVermelho = 440; // nota A
+        private const double FrequenciaBotaoAmarelo = 554.365; // nota C#
+        private const double FrequenciaBotaoAzul = 659.255; // nota E
+
+        private void AcionarBotao(Button btn, Color cor, double frequencia)
+        {
+            AlterarCorBotao(btn, cor, true);
+            Application.DoEvents();
+
+            TocarNotaMusical(frequencia);
+
+            AlterarCorBotao(btn, cor, false);
+            Application.DoEvents();
+        }
+
+        private void AlterarCorBotao(Button bt, Color cor, bool aplicarLight)
+        {
+            if (aplicarLight)
+            {
+                cor = ControlPaint.LightLight(cor);
+            }
+
+            bt.BackColor = cor;
+        }
+
+        private void TocarNotaMusical(double frequencia)
+        {
+            var nota = new SignalGenerator()
+            {
+                Gain = 0.2,
+                Frequency = frequencia,
+                Type = SignalGeneratorType.Triangle
+            }
+            .Take(TimeSpan.FromSeconds(0.5));
+
+            using (var wo = new WaveOutEvent())
+            {
+                wo.Init(nota);
+                wo.Play();
+                while (wo.PlaybackState == PlaybackState.Playing)
+                {
+                    Thread.Sleep(100);
+                    Application.DoEvents();
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            NovoJogo();
+        }
+
+        private void btnVermelho_Click(object sender, EventArgs e)
+        {
+            BotoesAcionadosJogador.Add(2);
+            AcionarBotao(btnVermelho, Color.Red, FrequenciaBotaoVermelho);
+        }
+
+        private void btnAzul_Click(object sender, EventArgs e)
+        {
+            BotoesAcionadosJogador.Add(4);
+            AcionarBotao(btnAzul, Color.Blue, FrequenciaBotaoAzul);
+        }
+
+        private void btnVerde_Click(object sender, EventArgs e)
+        {
+            BotoesAcionadosJogador.Add(1);
+            AcionarBotao(btnVerde, Color.Green, FrequenciaBotaoVerde);
+        }
+
+        private void btnAmarelo_Click(object sender, EventArgs e)
+        {
+            BotoesAcionadosJogador.Add(3);
+            AcionarBotao(btnAmarelo, Color.Yellow, FrequenciaBotaoAmarelo);
+        }
+
+        #region jogo
+
+        private int JogadorVez = 0;// 0 = cpu, 1 = jogador 1
+        private List<int> BotoesAcionadosJogo = new List<int>();
+        private List<int> BotoesAcionadosJogador = new List<int>();
+        private int EstadoJogo = 0;// 0 parado, 1 = jogando, 99 = gameover;
+
+        private void NovoJogo()
+        {
+            JogadorVez = 0;
+            EstadoJogo = 1;
+
+            while (EstadoJogo == 1)
+            {
+                LoopJogo();
+                Application.DoEvents();
+                Thread.Sleep(100);
+            }
+
+            Application.DoEvents();
+        }
+
+        private void LoopJogo()
+        {
+            switch (JogadorVez)
+            {
+                case 0:
+                    lbJogadorVez.Text = "Computador jogando";
+                    JogadaMaquina();
+                    break;
+
+                default:
+                    lbJogadorVez.Text = "Jogador " + JogadorVez;
+                    JogadaJogador(JogadorVez);
+                    break;
+            }
+        }
+
+        private void JogadaJogador(int jogador)
+        {
+            int indiceLista = 0;
+            foreach (var botaoJogador in BotoesAcionadosJogador)
+            {
+                if (BotoesAcionadosJogo[indiceLista] != botaoJogador)
+                {
+                    //jogador errou
+                    // fim de jogo
+                    EstadoJogo = 99;
+                }
+
+                if (indiceLista <= BotoesAcionadosJogo.Count)
+                {
+                    indiceLista++;
+                }
+            }
+
+            if (EstadoJogo == 99)
+            {
+                FimDeJogo();
+            }
+
+            if (BotoesAcionadosJogador.Count == BotoesAcionadosJogo.Count)
+            {
+                JogadorVez = 0;
+                BotoesAcionadosJogador.Clear();
+            }
+        }
+
+        private void JogadaMaquina()
+        {
+            //quantidadeToques++;
+            Random rnd = new Random();
+
+            // passa pelos que já estão na memória
+            foreach (var item in BotoesAcionadosJogo)
+            {
+                switch (item)
+                {
+                    case 1: // verde
+                        AcionarBotao(btnVerde, Color.Green, FrequenciaBotaoVerde);
+                        break;
+
+                    case 2: // vermelho
+                        AcionarBotao(btnVermelho, Color.Red, FrequenciaBotaoVermelho);
+                        break;
+
+                    case 3: // amarelo
+                        AcionarBotao(btnAmarelo, Color.Yellow, FrequenciaBotaoAmarelo);
+                        break;
+
+                    case 4: // azul
+                        AcionarBotao(btnAzul, Color.Blue, FrequenciaBotaoAzul);
+                        break;
+                }
+            }
+
+            // adiciona mais um a cada jogada
+            int numeroCor = rnd.Next(1, 5);
+            switch (numeroCor)
+            {
+                case 1: // verde
+                    BotoesAcionadosJogo.Add(1);
+                    AcionarBotao(btnVerde, Color.Green, FrequenciaBotaoVerde);
+                    break;
+
+                case 2: // vermelho
+                    BotoesAcionadosJogo.Add(2);
+                    AcionarBotao(btnVermelho, Color.Red, FrequenciaBotaoVermelho);
+                    break;
+
+                case 3: // amarelo
+                    BotoesAcionadosJogo.Add(3);
+                    AcionarBotao(btnAmarelo, Color.Yellow, FrequenciaBotaoAmarelo);
+                    break;
+
+                case 4: // azul
+                    BotoesAcionadosJogo.Add(4);
+                    AcionarBotao(btnAzul, Color.Blue, FrequenciaBotaoAzul);
+                    break;
+            }
+
+            JogadorVez = 1;
+            BotoesAcionadosJogador = new List<int>();
+        }
+
+        private void FimDeJogo()
+        {
+            EstadoJogo = 0;
+            lbJogadorVez.Text = "Fim de Jogo";
+            BotoesAcionadosJogo.Clear();
+            BotoesAcionadosJogador.Clear();
+        }
+
+        #endregion jogo
+    }
+}
